9fb3affc81ae953b18905ac8dff95c0898e01e50
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-07-17 11:12:36 -0300

    memory leak caused by RocketChat.settings.get

--- packages/rocketchat-lib/server/functions/checkUsernameAvailability.js
+++ packages/rocketchat-lib/server/functions/checkUsernameAvailability.js
@@ -2,22 +2,26 @@ import _ from 'underscore';
 import s from 'underscore.string';
 
 RocketChat.checkUsernameAvailability = function(username) {
-	return RocketChat.settings.get('Accounts_BlockedUsernameList', function(key, value) {
-		const usernameBlackList = _.map(value.split(','), function(username) {
-			return username.trim();
-		});
-		if (usernameBlackList.length !== 0) {
-			if (usernameBlackList.every(restrictedUsername => {
-				const regex = new RegExp(`^${ s.escapeRegExp(restrictedUsername) }$`, 'i');
-				return !regex.test(s.trim(s.escapeRegExp(username)));
-			})) {
-				return !Meteor.users.findOne({
-					username: {
-						$regex: new RegExp(`^${ s.trim(s.escapeRegExp(username)) }$`, 'i')
-					}
-				});
-			}
-			return false;
-		}
+	const value = RocketChat.settings.get('Accounts_BlockedUsernameList').trim();
+
+	if(value.length === 0) {
+		return true;
+	}
+
+	const usernameBlackList = _.map(value.split(','), function(username) {
+		return username.trim();
 	});
+	if (usernameBlackList.length !== 0) {
+		if (usernameBlackList.every(restrictedUsername => {
+			const regex = new RegExp(`^${ s.escapeRegExp(restrictedUsername) }$`, 'i');
+			return !regex.test(s.trim(s.escapeRegExp(username)));
+		})) {
+			return !Meteor.users.findOne({
+				username: {
+					$regex: new RegExp(`^${ s.trim(s.escapeRegExp(username)) }$`, 'i')
+				}
+			});
+		}
+	}
+	return false;
 };
